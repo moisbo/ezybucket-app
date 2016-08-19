c24afa10667015fbcaae559b4c7d7cbb4bf20967
Moises Sacal <moisbo@gmail.com>
2016-08-19 17:06:24 +1000

    added base app

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname + '/src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+});
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        filename: "index_bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
+            { test: /\.css$/, loader: "style-loader!css-loader"},
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+        ]
+    },
+    plugins: [
+        HtmlWebpackPluginConfig
+    ]
+};
